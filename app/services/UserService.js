0503f682c50767125583e4587e62a14c7ad8a961
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-23 18:15:09 -0500

    Added getIfo method to UserService

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,13 @@ class UserService {
     static create(id, username, name) {
         return new User(id, username, name, "Sin bio")
     }
+    
+    static getInfo(user) {
+        const userInfo = Object.keys(user).map((key) => {
+            return user[key]
+        })
+        return userInfo
+    }
 }
 
 module.exports = UserService
