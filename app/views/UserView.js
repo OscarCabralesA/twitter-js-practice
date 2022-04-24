fba3886fa9bc31e1084b8a215454b3f070b0db79
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-23 19:28:45 -0500

    Added full functionality to createUserMethod from UserView

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -5,6 +5,13 @@ class UserView {
         if (payload == null) {
             return {error: "payload no existe"}
         }
+        if (! (payload.hasOwnProperty('name') && payload.hasOwnProperty('username') && payload.hasOwnProperty('id')) ) {
+            return {error: "necesitan tener un valor válido"}
+        }
+        if (! (typeof(payload.id) === 'number' && typeof(payload.username) === 'string' && typeof(payload.username) === 'string')) {
+            return {error: "necesitan tener un valor válido"}
+        }
+        return UserService.create(payload.id, payload.username, payload.name)
     }
 }
 
